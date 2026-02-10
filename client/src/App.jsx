cb2341a0638260a5a04d2f8c19d2e7e8009e60e7
AIEC-ERP <raman@atlas-om.com>
2026-02-10 16:58:36 +0400

    frontend folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './Login'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Login/>
-      </>
-  )
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        deserunt quod accusantium vero, corporis expedita aut cumque harum dicta
+        porro ducimus eaque doloremque adipisci maiores sed veritatis sint quasi
+        repellendus dolore deleniti recusandae! Consequuntur, officia sed.
+        Accusamus provident laudantium, voluptate animi optio eum maiores
+        reiciendis quisquam, ipsam quae libero dolore deleniti dignissimos error
+        dolor, id quibusdam repellat tenetur quidem excepturi. Saepe
+        necessitatibus minus voluptate delectus sint praesentium explicabo
+        ducimus aliquid, nobis fuga vero minima tempore error doloremque dolorum
+        quo dolorem quaerat quam laboriosam. Velit molestiae nisi eligendi?
+        Reprehenderit maxime deleniti ipsam repudiandae ea amet enim quia alias
+        totam! Perspiciatis pariatur illo iste a quasi vero, nisi accusantium
+        tenetur in. Soluta, perspiciatis facere! Quam deleniti minima molestiae
+        quidem provident aperiam hic nemo delectus soluta incidunt odio repellat
+        est minus commodi ea labore tempore nihil dolore ab, vitae laboriosam
+        odit expedita. Fugit amet, voluptatum ipsam, numquam necessitatibus
+        quibusdam totam aut possimus dolores tempora, corporis aliquid vitae
+        saepe molestias odio. Nostrum voluptatum, mollitia laboriosam voluptates
+        perferendis tempora iure sint est possimus voluptatibus nobis aperiam
+        numquam optio ipsam praesentium a iste alias, veritatis culpa dolorem
+        obcaecati labore recusandae. Odit beatae eveniet non laborum, doloremque
+        labore reprehenderit dolore. Fugiat quae magni repellat autem temporibus
+        nemo.
+      </p>
+    </>
+  );
 }
 
-export default App
+export default App;
